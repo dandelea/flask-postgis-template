e684e3efa0ce9dafd1e30243578e7c342cc9ec02
Daniel <dandelea@users.noreply.github.com>
2018-01-22 10:01:42 +0100

    Update converters.py

--- project/database/converters.py
+++ project/database/converters.py
@@ -9,6 +9,7 @@ def Postals_converter(filters=None, sort=None, group_by=None, single=None, **kw)
 		object["the_geom"] = GeoJSONtoJSON(object["the_geom"])
 		paystats = object["paystats"]
 		total_amount = sum([paystat["amount"] for paystat in paystats])
+		object["the_geom"]["properties"] = {}
 		object["the_geom"]["properties"]["paystats"] = paystats
 		object["the_geom"]["properties"]["total_amount"] = total_amount
 		del object["paystats"]
@@ -17,6 +18,7 @@ def Postal_converter(filters=None, sort=None, group_by=None, single=None, **kw):
 	kw["result"]["the_geom"] = GeoJSONtoJSON(kw["result"]["the_geom"])
 	paystats = kw["result"]["paystats"]
 	total_amount = sum([paystat["amount"] for paystat in paystats])
+	kw["result"]["the_geom"]["properties"] = {}
 	kw["result"]["the_geom"]["properties"]["paystats"] = paystats
 	kw["result"]["the_geom"]["properties"]["total_amount"] = total_amount
 	del kw["result"]["paystats"]
